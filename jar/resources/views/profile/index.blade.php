71de12bb221be622b2a793d230665fada081a628
Mohammed Mostafa <tayar.shaeby@gmail.com>
2026-01-05 23:36:13 +0200

    fix: update product route from my-products.create to my-products.index

--- jar/resources/views/profile/index.blade.php
+++ jar/resources/views/profile/index.blade.php
@@ -511,7 +511,7 @@
                 <a href="{{ route('chat') }}" title="المراسلات">
                     <i class="fas fa-comments"></i> المراسلات
                 </a>
-                <a href="{{ route('my-products.create') }}" title="إدارة المنتجات">
+                <a href="{{ route('my-products.index') }}" title="إدارة المنتجات">
                     <i class="fas fa-box"></i> إدارة المنتجات
                 </a>
                 <a href="{{ route('profile.support-tickets') }}" title="طلبات إرجاع المنتجة">
